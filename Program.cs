c6c591d654e3a66f2770efe2f8d6e74daf956406
wasif farid <wsffarid1@gmail.com>
2021-09-29 17:46:43 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GreatestVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
