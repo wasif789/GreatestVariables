e658ab9e0f6a6eedc78eec86904a682159493a57
wasif farid <wsffarid1@gmail.com>
2021-09-29 17:53:30 +0530

    greatest of 3 integer

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace GreatestVariables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GreatestNum obj = new GreatestNum();
+            //Check greatest number among three number
+            Console.WriteLine("Welcome to generics program!");
+            int number1, number2, number3;
+            Console.WriteLine("Enter number 1");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 3");
+            number3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Greatest number is {0} ", obj.LargestNumber(number1, number2, number3));
+
         }
     }
 }
