15e0196d21ce1e977ab16ad306c696cbcb84836c
wasif farid <wsffarid1@gmail.com>
2021-09-29 18:00:39 +0530

    greatest of 3 float

--- GreatestNum.cs
+++ GreatestNum.cs
@@ -26,5 +26,25 @@ namespace GreatestVariables
                 return 0;
             }
         }
+
+        //Method to compare which is greater in floattype
+        public float LargestNumberFloat(float number1, float number2, float number3)
+        {
+            if ((number1.CompareTo(number2) > 0) && (number1.CompareTo(number3) > 0))
+            {
+                return number1;
+            }
+            else if ((number2.CompareTo(number1) > 0) && (number2.CompareTo(number3) > 0))
+            {
+                return number2;
+            }
+            else if ((number3.CompareTo(number2) > 0) && (number3.CompareTo(number1) > 0))
+            {
+                return number3;
+            }
+            else return default;
+
+        }
     }
+    
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace GreatestVariables
         {
             GreatestNum obj = new GreatestNum();
             //Check greatest number among three number
-            Console.WriteLine("Welcome to generics program!");
+            Console.WriteLine("****INT DATATYPE****");
             int number1, number2, number3;
             Console.WriteLine("Enter number 1");
             number1 = Convert.ToInt32(Console.ReadLine());
@@ -18,6 +18,16 @@ namespace GreatestVariables
             number3 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Greatest number is {0} ", obj.LargestNumber(number1, number2, number3));
 
+            //Check greatest number among three number
+            Console.WriteLine("****FLOAT DATATYPE****");
+            float numberf1, numberf2, numberf3;
+            Console.WriteLine("Enter number 1");
+            numberf1 = (float)Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter number 2");
+            numberf2 = (float)Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter number 3");
+            numberf3 = (float)Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Greatest number is {0} ", obj.LargestNumberFloat(numberf1, numberf2, numberf3));
         }
     }
 }
